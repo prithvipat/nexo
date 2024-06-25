3daf611534ff7f45600f606d40171f7ae4b87854
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-06-25 15:08:27 -0400

    Update models.py

--- user/models.py
+++ user/models.py
@@ -6,7 +6,6 @@ from datetime import datetime
 
 User = get_user_model()
 
-
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -18,12 +17,26 @@ class Profile(models.Model):
 
 
 class Posts(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id_post = models.UUIDField(primary_key=True, default=uuid.uuid4)
     user = models.CharField(max_length=100)
     image = models.ImageField(upload_to='post_images')
     caption = models.TextField()
     created_at = models.DateTimeField(default=datetime.now)
     likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
+
+class FollowerCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
 
     def __str__(self):
         return self.user
