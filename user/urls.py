6c962095b4999b849ba0bc136cad57bd4b1c8d8d
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-06-25 15:25:09 -0400

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('settings/', views.settings, name='settings'),
+    path('settings', views.settings, name='settings'),
     path('signup', views.signup, name='signup'),
     path('login', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('post/', views.post, name='post'),
+    path('logout', views.logout, name='logout'),
+    path('post', views.post, name='post'),
     path('like-post', views.like_post, name='like_post'),
-    path('profile/<str:pk>/', views.profile, name='profile'),
-    path('follow/', views.follow, name='follow'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('follow', views.follow, name='follow'),
 ]
