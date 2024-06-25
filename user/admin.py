848ecf8bdf47d52332d4a0a5958933b38e3a89ef
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-06-25 15:08:47 -0400

    Update admin.py

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Profile, Posts
+from .models import Profile, Posts, LikePost, FollowerCount
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Posts)
+admin.site.register(LikePost)
+admin.site.register(FollowerCount)
