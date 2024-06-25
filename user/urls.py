61c78a7c68ec0187453ff1c297825833734e2da7
Prithvi Paturi <87906760+prithvipat@users.noreply.github.com>
2024-06-25 15:09:06 -0400

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
-    path('settings/', views.settings, name='settings'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('profile/',views.profile, name='profile'),
-    path('upload/', views.upload, name='upload')
+    path('settings', views.settings, name='settings'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('post', views.post, name='post'),
+    path('like-post', views.like_post, name='like_post'),
+    path('profile/<str:pk>/', views.profile, name='profile'),
+    path('follow/', views.follow, name='follow'),
 ]
